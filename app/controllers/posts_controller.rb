d2fbcb3be8327be06f8903a053cf7dc90b14f0dd
namitha chandran <namithachandran003@gmail.com>
2019-03-01 15:49:33 +0530

    Added Update Feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+def edit
+  @post = Post.find(params[:id])
+end
+
 
   def create
     @post = Post.new(post_params)
@@ -20,6 +24,16 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+  @post = Post.find(params[:id])
+ 
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
 
   private
 
