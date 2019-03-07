68b456fd65c3540eeba15c10026db9a343188ba8
namitha chandran <namithachandran003@gmail.com>
2019-03-07 15:43:32 +0530

    Adding user authorization for delete and update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 # post_controller
 class PostsController < ApplicationController
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def show
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 # post
 class Post < ApplicationRecord
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
 end
