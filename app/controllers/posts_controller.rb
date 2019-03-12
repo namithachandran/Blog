bdfc8d2d80a3895fc65681ead570eff9fbb5bded
namitha chandran <namithachandran003@gmail.com>
2019-03-12 12:20:43 +0530

    Added image upload option

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,6 +43,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :text)
+    params.require(:post).permit(:title, :text, :image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_090136) do
+ActiveRecord::Schema.define(version: 2019_03_12_045957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_03_07_090136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
